69a84b57a5e2ab3c29a57983e091ac5431826196
Agneta <agnetagsu@yahoo.es>
2021-05-22 21:25:01 +0200

    Agregar archivos de proyecto.

---
+++ Flujo de datos - C2 Ej16/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Flujo_de_datos___C2_Ej16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double precio;
+            double preciofinal;
+            const double IVA = 21;
+
+            Console.WriteLine("Introduce el precio:");
+            precio = Convert.ToDouble(Console.ReadLine());
+            preciofinal = precio*(1+IVA/100);
+            Console.WriteLine("El PRECIO con IVA ES " + preciofinal);
+        }
+    }
+}
